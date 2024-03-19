14f3ed493e2e41646b739a22e8b2059194afaab0
khairah <khaeraholamide@gmail.com>
2024-03-19 13:21:57 +0100

    assignment
    looping through objects and updating their properties

---
+++ week-3/assignment.js
@@ -0,0 +1,55 @@
+const pen ={
+    name : 'montBlanc',
+    color : 'black and gold',
+    type : 'ceramic',
+    inkColor : 'black',
+    companyLocation: 'france'
+}
+
+const student ={
+    name : 'mama',
+    age : 16,
+    favouriteSubject : 'Math',
+    gender : 'female',
+    nationality : 'nigeria'
+}
+
+const house ={
+    type : 'bungalow',
+    color : 'offWhite',
+    material : 'block',
+    location:  'abuja', 
+
+}
+
+const phone ={
+    type : 'samsung',
+    color : 'black',
+    companyLocation : 'korea',
+    model : 's23',
+
+}
+
+const book = {
+    type : 'textbook',
+    color : 'blue',
+    page : 23,
+    author : 'khairah',
+
+}
+
+const food ={
+    name : 'rice',
+    type : 'jollof',
+    class : 'carbohydrate',
+    benefit : 'energy production',
+    mode : 'gas,fireWood,stove'
+}
+
+const school = {
+    name : 'codeCampus',
+    location : 'wuse',
+    state : 'abuja',
+}
+
+ 
---
+++ week-3/object/object-loop.js
@@ -0,0 +1,22 @@
+const pserson ={
+    fnamw:"john",
+    lname: "doe",
+    age:25
+}
+
+console.log(person)
+// let txt = " ";
+// for (let x in person ){
+//     txt = txt + person [x] + " ";
+// }
+// console.log(txt)
+
+// let txt =" ";
+// txt = txt + "hello"
+// console.log(txt)
+ 
+
+person.nationality = 'Nigerian'
+console.log(person)
+delete person.nationality;
+console.log(personalbar)
