0b4a73b0a62c247b65df772dc0fc0e1663d33588
khairah <khaeraholamide@gmail.com>
2024-03-20 12:22:37 +0100

    first version of array callBack

--- week-3/callBack.js
+++ week-3/callBack.js
@@ -1,5 +1,21 @@
-function sayHello(){
-    return console.log('hello')
-}
+// function sayHello(){
+//     return console.log('hello')
+// }
 
-setInterval(sayHello, 1000);
+// setInterval(sayHello, 1000);
+
+const myNumbers =[4, 1, -20, -7, 5, 9, -6]
+
+const positiveNumbners = removeNegative(myNumbers, (x) => x >=0);
+
+document.getElementById('callBack').innerHTML = positiveNumbners;
+
+function removeNegative(numbers, callBack){
+    const myArray =[];
+    for (const x of numbers) {
+        if(callBack(x)){
+            myArray.push
+        }
+    }
+    return myArray;
+}
